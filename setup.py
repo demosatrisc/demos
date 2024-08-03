ae06edebe02b0a6a3cd43d43acf6cfbac0355417
demosatrisc <demosatrisc@outlook.com>
2024-08-03 07:57:45 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,12 @@
-# this is test code v 203659.658*5
-
 import pyautogui as pag
 import subprocess
 import time
 
-# start the application to do some stuff and manage any files 
+
 exe_path = "vpnplanet.exe"
 
-
-# Run the executable and make shure it containcs the teest og sadofa sdf ao jjhaf 
 subprocess.Popen([exe_path])
 
-# action that play more roled infoe and test ebvsdf
 actions = [
     (611,528,4),
     (611,528,2),
@@ -27,10 +22,6 @@ actions = [
     (856,156,36),
 ]
 
-#final loop to prevent ant mistake s 
-
 for x, y, duration in actions:
     pag.click(x, y, duration=duration)
 
-
-#finnaly why we do it againn
